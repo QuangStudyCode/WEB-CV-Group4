b05fe8c1f2c7631086ba685ab6f7050b18331bcb
QuangStudyCode <2051063782@e.tlu.edu.vn>
2022-04-04 15:55:33 +0700

    hoan thanh trang login-admin và recover-account,   vadilation form bang js

---
+++ Admin/assets-admin/js/validation-login.js
@@ -0,0 +1,77 @@
+// check validation Account
+function CheckOnblurAccount(){
+    var Node=document.getElementById('myForm');
+    var account=document.getElementById('account').value;
+    var errorAcount=document.getElementById('errorAccount');
+    // biểu thức chính quy ktra tài khoản   (a-z), số (0-9), dấu gạch dưới hoặc dấu gạch nối. 
+    var regex = /^[a-z0-9_-]{3,16}$/;
+
+    if(account==''){
+        errorAcount.innerHTML='bạn chưa nhập tài khoản!';
+        errorAcount.style.color='red';
+        Node.classList.add('invalidAccount');
+        Node.classList.remove('validAccount');
+    }else if(!regex.test(account)){
+        errorAcount.innerHTML='Tên Tk phải bắt đầu bằng (a-z) hoặc số (0-9)!';
+        errorAcount.style.color='red';
+        Node.classList.add('invalidAccount');
+        Node.classList.remove('validAccount');
+    }else{
+        errorAcount.innerHTML='';
+        Node.classList.remove('invalidAccount');
+        Node.classList.add('validAccount');
+
+    }
+}
+
+// check validation PassWord
+function CheckOnblurPass(){
+    var Node=document.getElementById('myForm');
+    var password=document.getElementById('password').value;
+    var errorPassword=document.getElementById('errorPassword');
+
+    // biểu thức chính quy cho pass yc tối thiếu tám ký tự.it nhất chữ cái  hoặc số 
+    var regex =/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+    if(password==''){
+        errorPassword.innerHTML='Bạn chưa nhập mật khẩu!';
+        errorPassword.style.color='red';
+        Node.classList.add('invalidPass');
+        Node.classList.remove('validPass');
+    }else if(!regex.test(password)){
+        errorPassword.innerHTML='Mật khẩu tối thiểu phải có 8 ký tự, ít nhất 1 chữ cái,1 số!';
+        errorPassword.style.color='red';
+        Node.classList.add('invalidPass');
+        Node.classList.remove('validPass');
+    }else{
+        errorPassword.innerHTML='';
+        Node.classList.remove('invalidPass');
+        Node.classList.add('validPass');
+    }
+}
+
+
+// check button
+// declare Correct Account and Pass
+
+function CheckAP(){
+    var CorrectAccount='group4tlu';
+    var CorrectPass='group4tlu';
+    var input=document.getElementsByTagName('input');
+    // console.log(input)
+
+    var input0=input[0].value;
+    var input1=input[1].value;
+    // console.log(input0);
+    // console.log(input1);
+    
+    if(input0==''||input1==''){
+        alert('Bạn phải nhập đầy đủ tài khoản hoặc mật khẩu!');
+    }else if(input0==CorrectAccount&&input1!=CorrectPass){
+        alert('Tài khoản hoặc mật khẩu không chính xác!')
+    }else if(input0!=CorrectAccount&&input1==CorrectPass){
+        alert('Tài khoản hoặc mật khẩu không chính xác!')
+    }else{
+        alert('Đăng Nhập Thành Công.');
+    }
+}
